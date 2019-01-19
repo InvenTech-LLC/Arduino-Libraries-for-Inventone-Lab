a488f882a14c1f37d67ad9e0322a777c1e359916
OladapoAjala <ajalaoladapoemmanuel.ao@gmail.com>
2019-01-19 01:46:59 +0100

    Updated arduino libraries for inventone

--- src/inventoneACT/inventoneACT.cpp
+++ src/inventoneACT/inventoneACT.cpp
@@ -1,12 +1,32 @@
 #include "inventoneACT.h"
 #include "Servo.h"
 
+// #define GREATERTHAN >
+// #define GREATERTHANOREQUAL >=
+// #define EQUALTO ==
+// #define NOTEQUALTO !=
+// #define LESSTHAN <
+// #define LESSTHANOREQUAL <=
+
+
 Servo myservo;
 
 inventoneACT::inventoneACT(uint8_t pin) {
     pinMode(pin, OUTPUT);
     myservo.attach(pin);
     _pin = pin;
+}
+
+inventoneACT::inventoneACT() {
+    delay(10);
+}
+
+void inventoneACT::setupForRead(uint8_t pin) {
+    pinMode(pin, INPUT);
+}
+
+void inventoneACT::setupForWrite(uint8_t pin) {
+    pinMode(pin, OUTPUT);
 }
 
 void inventoneACT::turnLedOn() {
@@ -31,4 +51,98 @@ void inventoneACT::rotate(int speed){
 
 void inventoneACT::turn(int degrees) {
     myservo.write(degrees);
+}
+
+float inventoneACT::multiplex(float data1, float data2, char *condition) {
+    
+    if (strcmp(condition, ">") == 0 ) {
+        if (data1 > data2) {
+            return data1;
+        }
+        else return data2;
+    }
+    else if (strcmp(condition, ">=") == 0) {
+        if (data1 >= data2) {
+            return data1;
+        }
+        else return data2;
+    }
+    else if (strcmp(condition, "<") == 0) {
+        if (data1 < data2) {
+            return data1;
+        }
+        else return data2;
+    }
+    else if (strcmp(condition, "<=") == 0) {
+        if (data1 <= data2) {
+            return data1;
+        }
+        else return data2;
+    }
+
+}
+
+float inventoneACT::pass (float input, bool boolean) {
+    if (boolean) {
+        return input;
+    }
+    else return 0;
+}
+
+bool inventoneACT::threshold(float data1, float data2, char *condition) {
+
+    if (strcmp(condition, ">") == 0 ) {
+        if (data1 > data2) {
+            return true;
+        }
+        else return false;
+    }
+    else if (strcmp(condition, ">=") == 0) {
+        if (data1 >= data2) {
+            return true;
+        }
+        else return false;
+    }
+    else if (strcmp(condition, "<") == 0) {
+        if (data1 < data2) {
+            return true;
+        }
+        else return false;
+    }
+    else if (strcmp(condition, "<=") == 0) {
+        if (data1 <= data2) {
+            return true;
+        }
+        else return false;
+    }
+}
+
+int inventoneACT::map(long data, long in_min, long in_max, long out_min, long out_max) {
+   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+char* inventoneACT::string(char *text) {
+    return text;
+}
+
+char* inventoneACT::buildText(char *text1, float x, char *text2) {
+    char *string;
+    sprintf (string, "%s %d %s", text1, x, text2);
+    return string;
+}
+
+int inventoneACT::analog_read(uint8_t pin) {
+    return analogRead(pin);
+}
+
+void inventoneACT::analog_write(uint8t_t pin) {
+    analogWrite(pin);
+}
+
+bool inventoneACT::digital_read(uint8_t pin) {
+    return digitalRead(pin);
+}
+
+void inventoneACT::digital_write(uint8_t pin) {
+    digitalWrite(pin);
 }
--- src/inventoneACT/inventoneACT.h
+++ src/inventoneACT/inventoneACT.h
@@ -7,12 +7,25 @@ class inventoneACT
 {
     public:
         inventoneACT (uint8_t pin);
+        inventoneACT ();
+        void setupForRead (uint8_t pin);
+        void setupForWrite (uint8_t pin);
         void turnLedOn ();
         void turnLedOff ();
         void turnRelayOn ();
         void turnRelayOff ();
         void rotate(int speed);
         void turn(int degrees);
+        float multiplex(float data1, float data2, char *conditon);
+        float pass(float input, bool boolean);
+        bool threshold(float data1, float data2, char *condition);
+        int map(long data, int in_min, int in_max, int out_min, int out_max); 
+        char *string(char *text);
+        char *buildText(char *text1, float x, char *text2);
+        int analog_read(uint8_t pin);
+        void analog_write(uint8_t pin);
+        bool digital_read(uint8_t pin);
+        void digital_write(uint8_t pin);
     private:
         uint8_t _pin;
 };
